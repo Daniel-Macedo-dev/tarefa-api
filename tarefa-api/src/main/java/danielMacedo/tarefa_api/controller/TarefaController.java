5cadc9fb606803076f22b24c34ac835c933b7eac
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-08-30 11:16:29 -0300

    Add Controller

--- tarefa-api/src/main/java/danielMacedo/tarefa_api/controller/TarefaController.java
+++ tarefa-api/src/main/java/danielMacedo/tarefa_api/controller/TarefaController.java
@@ -1,9 +1,47 @@
 package danielMacedo.tarefa_api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import danielMacedo.tarefa_api.business.TarefaService;
+import danielMacedo.tarefa_api.infrastructure.entities.Tarefa;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tarefa")
+@RequiredArgsConstructor
 public class TarefaController {
+
+    private final TarefaService _tarefaService;
+
+    @PostMapping
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Tarefa tarefa){
+        _tarefaService.salvarTarefa(tarefa);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Tarefa> buscarTarefaPorId(@RequestParam Integer id){
+        return ResponseEntity.ok(_tarefaService.buscarTarefaPorId(id));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarTarefaPorId(@RequestParam Integer id){
+        _tarefaService.deletarTarefaPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tarefa> substituirTarefa(
+            @RequestParam Integer id,
+            @RequestBody Tarefa novaTarefa){
+        _tarefaService.substituirTarefa(id, novaTarefa);
+        return ResponseEntity.ok().build();
+    }
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Tarefa> atualizarTarefa(
+            @RequestParam Integer id,
+            @RequestBody Tarefa novaTarefa){
+        _tarefaService.atualizarTarefa(id, novaTarefa);
+        return ResponseEntity.ok().build();
+    }
 }

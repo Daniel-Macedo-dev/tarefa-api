61c0b06e4f6b7cebe2e05557d6ae8f3d43580d6e
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-09-07 13:10:11 -0300

    Ajustes

--- tarefa-api/src/main/java/danielMacedo/tarefa_api/business/TarefaService.java
+++ tarefa-api/src/main/java/danielMacedo/tarefa_api/business/TarefaService.java
@@ -45,7 +45,6 @@ public class TarefaService {
         return _tarefaRepository.saveAndFlush(tarefaBusca);
     }
 
-    //delete by id
     public void deletarTarefaPorId(Integer _id){
         _tarefaRepository.deleteById(_id);
     }

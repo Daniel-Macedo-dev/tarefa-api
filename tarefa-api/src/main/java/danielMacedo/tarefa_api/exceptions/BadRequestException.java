55f96772f0b83517b31e99d5893f4c3df464d966
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-01-26 19:45:28 -0300

    Add exceptions

---
+++ tarefa-api/src/main/java/danielMacedo/tarefa_api/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package danielMacedo.tarefa_api.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}

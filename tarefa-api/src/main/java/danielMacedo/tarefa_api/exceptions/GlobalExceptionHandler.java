4d73602238911fea00e940581cc45c8351bcbe12
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-03-17 18:38:59 -0300

    Add controller advice

--- tarefa-api/src/main/java/danielMacedo/tarefa_api/exceptions/GlobalExceptionHandler.java
+++ tarefa-api/src/main/java/danielMacedo/tarefa_api/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package danielMacedo.tarefa_api.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
